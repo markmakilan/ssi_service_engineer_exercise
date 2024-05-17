92cc58f3c1fb2c8327fac072ddd0f6fae50b842c
Mark Makilan <mark.makilan@gets.ph>
2024-05-17 16:39:43 +0800

    fix current date time

--- memory_check.sh
+++ memory_check.sh
@@ -12,17 +12,17 @@ datetime() {
 
 # Show needed parameters 
 usage() {
-    echo "Usage: $0 -c <critical threshold> -w <warning threshold> -e <email>"
+    echo "$(datetime) Usage: $0 -c <critical threshold> -w <warning threshold> -e <email>"
     exit_with_info 1
 }
 
 # Show exit code message
 exit_with_info() {
     case $1 in
-        0) echo "${ datetime } Info: Exiting with success (exit code $1)." ;;
-        1) echo "${ datetime } Info: Exiting with warning (exit code $1)." ;;
-        2) echo "${ datetime } Info: Exiting with critical error (exit code $1)." ;;
-        *) echo "${ datetime } Info: Exiting with unknown error." ;;
+        0) echo "$(datetime) Info: Exiting with success (exit code $1)." ;;
+        1) echo "$(datetime) Info: Exiting with warning (exit code $1)." ;;
+        2) echo "$(datetime) Info: Exiting with critical error (exit code $1)." ;;
+        *) echo "$(datetime) Info: Exiting with unknown error." ;;
     esac
 
     exit $1
@@ -52,7 +52,7 @@ done
 # Validate critical threshold and warning threshold
 # critical threshold must be greater than warning threshold
 if (( CRITICAL <= WARNING )); then
-    echo "Error: The critical threshold must be greater than warning threshold."
+    echo "$(datetime) Error: The critical threshold must be greater than warning threshold."
     usage
 fi
 
@@ -73,6 +73,6 @@ elif ((TOTAL_MEMORY_USAGE >= WARNING)); then
 fi
 
 # Print memory usage information
-echo "Info: Memory usage: $TOTAL_MEMORY_USAGE%"
+echo "$(datetime) Info: Memory usage: $TOTAL_MEMORY_USAGE%"
 
 exit_with_info $EXIT_CODE
